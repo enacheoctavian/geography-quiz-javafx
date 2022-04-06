723dd5a13046feacbeadbb57a0d9cef81768ae43
Enache Octavian <enacheioan11@yahoo.com>
2022-04-06 22:16:40 +0300

    Change app title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Geography Quiz");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
