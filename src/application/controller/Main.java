863ea7970ade7019c25ed04a4c86d94005e738dc
Enache Octavian <enacheioan11@yahoo.com>
2022-04-07 10:17:09 +0300

    Add welcome page

--- src/application/controller/Main.java
+++ src/application/controller/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package controller;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("welcome-page.fxml"));
         primaryStage.setTitle("Geography Quiz");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
     }
 
