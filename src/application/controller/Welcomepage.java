84f237c5fcd93aaa88bf53ce30586517840c24e0
Enache Octavian <enacheioan11@yahoo.com>
2022-04-08 19:39:22 +0300

    Add welcome page controller

---
+++ src/application/controller/Welcomepage.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Welcomepage {
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void onPressStart(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("login-page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
