48a4572dfffbff893c6b8fe3f0fe73fa76f92399
Enache Octavian <enacheioan11@yahoo.com>
2022-04-08 22:40:29 +0300

    Add models for questions and user

---
+++ src/application/model/question.java
@@ -0,0 +1,60 @@
+package model;
+
+//model for every question
+
+public class question {
+    private String question;
+    private String ans1;
+    private String ans2;
+    private String ans3;
+    private String ans4;
+    private int correctAns;
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAns1(String ans1) {
+        this.ans1 = ans1;
+    }
+
+    public void setAns2(String ans2) {
+        this.ans2 = ans2;
+    }
+
+    public void setAns3(String ans3) {
+        this.ans3 = ans3;
+    }
+
+    public void setAns4(String ans4) {
+        this.ans4 = ans4;
+    }
+
+    public void setCorrectAns(int correctAns) {
+        this.correctAns = correctAns;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAns1() {
+        return ans1;
+    }
+
+    public String getAns2() {
+        return ans2;
+    }
+
+    public String getAns3() {
+        return ans3;
+    }
+
+    public String getAns4() {
+        return ans4;
+    }
+
+    public int getCorrectAns() {
+        return correctAns;
+    }
+}
